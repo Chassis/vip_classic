5d30279dce856d80cdaa8678072de4e9e56528aa
Bronson Quick <bronson@bronsonquick.com.au>
2018-03-12 13:27:54 +1000

    Tabs not spaces https://www.youtube.com/watch?v=SsoOG6ZeyUI

--- modules/vip/manifests/init.pp
+++ modules/vip/manifests/init.pp
@@ -1,27 +1,27 @@
 # Automattic VIP Classic Beta extension for Chassis
 class vip (
-  $php = $::vip_config[php],
-  $path  = '/vagrant/extensions/vip-classic',
+		$php = $::vip_config[php],
+		$path  = '/vagrant/extensions/vip-classic',
 ) {
 
-  file { "${$path}/local-config.php":
-    content => template('vip/local-config.php.erb'),
-    owner   => 'www-data',
-    group   => 'www-data',
-    mode    => '0644',
-  }
+		file { "${$path}/local-config.php":
+			content => template('vip/local-config.php.erb'),
+			owner   => 'www-data',
+			group   => 'www-data',
+			mode    => '0644',
+		}
 
-  file { "${$path}/roles.php":
-    content => template('vip/roles.php.erb'),
-    owner   => 'www-data',
-    group   => 'www-data',
-    mode    => '0644',
-  }
+		file { "${$path}/roles.php":
+			content => template('vip/roles.php.erb'),
+			owner   => 'www-data',
+			group   => 'www-data',
+			mode    => '0644',
+		}
 
-  file { "${$path}/vip-config.php":
-    content => template('vip/vip-config.php.erb'),
-    owner   => 'www-data',
-    group   => 'www-data',
-    mode    => '0644',
-  }
+		file { "${$path}/vip-config.php":
+			content => template('vip/vip-config.php.erb'),
+			owner   => 'www-data',
+			group   => 'www-data',
+			mode    => '0644',
+		}
 }
