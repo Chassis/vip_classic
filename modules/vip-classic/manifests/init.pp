c1badaea6870414106c24349d65c783422e139ef
Bronson Quick <bronson@bronsonquick.com.au>
2018-03-12 15:16:27 +1000

    Move ensure to the top

--- modules/vip-classic/manifests/init.pp
+++ modules/vip-classic/manifests/init.pp
@@ -13,27 +13,27 @@ class vip-classic (
   }
 
   file { "${$path}/local-config.php":
+    ensure  => $file,
     content => template('vip-classic/local-config.php.erb'),
     owner   => 'www-data',
     group   => 'www-data',
-    mode    => '0644',
-    ensure  => $file
+    mode    => '0644'
   }
 
   file { "${$path}/roles.php":
+    ensure  => $file,
     content => template('vip-classic/roles.php.erb'),
     owner   => 'www-data',
     group   => 'www-data',
-    mode    => '0644',
-    ensure  => $file
+    mode    => '0644'
   }
 
   file { "${$path}/vip-config.php":
+    ensure  => $file,
     content => template('vip-classic/vip-config.php.erb'),
     owner   => 'www-data',
     group   => 'www-data',
-    mode    => '0644',
-    ensure  => $file
+    mode    => '0644'
   }
   class {'vip-classic::plugins': }
   class {'vip-classic::php':
